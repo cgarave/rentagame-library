1259675269aa1ec69487eb85e76415c55ee8d14a
Rave Flores <reyvflores40@gmail.com>
2026-07-02 14:53:05 +0800

    changes to homepage gamesContainer component logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,133 +6,77 @@ import { headers } from "next/headers";
 import prisma from "@/lib/prisma";
 import {GameDetails} from "@/types/GameDetails";
 import {AlertBasic} from "@/components/Alert";
+import Link from "next/link";
 
 export default async function Home() {
     const session = await auth.api.getSession({
         headers: await headers()
     });
 
-    if(!session?.user) {
-        const games = await getGames();
-        return (
-            <>
-                <main className={'flex flex-col gap-y-6 mt-30 px-3 xl:px-0 xl:w-[1280px] xl:mx-auto'}>
-                    {/*<AlertBasic title={'Announcement'}*/}
-                    {/*            description={['Effective today, we are removing the account deposit requirement. We realized that requiring a deposit goes against our goal of providing an affordable game rental service.',*/}
-                    {/*                        'For existing renters who have already paid an account deposit, your deposit will be refunded once your rental period expires.']}*/}
-                    {/*/>*/}
-                    <h1 className={'font-bold text-4xl'}>Browse Games</h1>
-                    <GamesContainer>
-                        {
-                            games.map((game: GameDetails) => (
-                                <GameCard key={game.id}
-                                          id={game.id}
-                                          gameImage={game.gameImage}
-                                          gameTitle={game.gameTitle}
-                                          weeklyPrice={game.weeklyPrice}
-                                          monthlyPrice={game.monthlyPrice}
-                                          availableTrophy={game.availableTrophy}
-                                          availableNonTrophy={game.availableNonTrophy}
-                                          renters={game.renters}
-                                          slot={game.slot}
-                                          includeButton={true}
-                                          includeBadge={false}
-                                          isGameReleased={game.isGameReleased}
-                                />
-                            ))
-                        }
-                    </GamesContainer>
-                </main>
-            </>
-        )
-    } else {
-        const notRentedGames = await prisma.game.findMany({
-            where: {
-                rentals: {
-                    none: {
-                        userId: session.user.id,
-                    },
+    const games = await getGames();
+    const notRentedGames = await prisma.game.findMany({
+        where: {
+            rentals: {
+                none: {
+                    userId: session?.user.id,
                 },
             },
-            orderBy: {
-                gameTitle: 'desc'
-            },
-        })
-        return (
-            <>
-                <main className={'flex flex-col gap-y-6 mt-30 px-3 xl:px-0 xl:w-[1280px] xl:mx-auto'}>
-                    {/*<AlertBasic title={'Announcement'}*/}
-                    {/*            description={['Effective today, we are removing the account deposit requirement. We realized that requiring a deposit goes against our goal of providing an affordable game rental service.',*/}
-                    {/*                'For existing renters who have already paid an account deposit, your deposit will be refunded once your rental period expires.']}*/}
-                    {/*/>*/}
-                    <h1 className={'font-bold text-4xl'}>Browse Games</h1>
-                    <GamesContainer>
-                        {
-                            notRentedGames.map((game: GameDetails) => (
-                                <GameCard key={game.id}
-                                          id={game.id}
-                                          gameImage={game.gameImage}
-                                          gameTitle={game.gameTitle}
-                                          weeklyPrice={game.weeklyPrice}
-                                          monthlyPrice={game.monthlyPrice}
-                                          availableTrophy={game.availableTrophy}
-                                          availableNonTrophy={game.availableNonTrophy}
-                                          renters={game.renters}
-                                          slot={game.slot}
-                                          includeButton={true}
-                                          includeBadge={false}
-                                          isGameReleased={game.isGameReleased}
-                                />
-                            ))
-                        }
-                    </GamesContainer>
-                </main>
-            </>
-        )
-    }
+        },
+        orderBy: {
+            gameTitle: 'desc'
+        },
+    })
 
-
-
-    // return (
-    //     <>
-    //         <main className={'flex flex-col gap-y-6 mt-30 px-3 xl:px-0 xl:w-[1280px] xl:mx-auto'}>
-    //             <h1 className={'font-bold text-4xl'}>Browse Games</h1>
-    //             <GamesContainer>
-    //                 {
-    //                     session.user ?
-    //                         notRentedGames.map(game => (
-    //                             <GameCard key={game.id}
-    //                                       id={game.id}
-    //                                       gameImage={game.gameImage}
-    //                                       gameTitle={game.gameTitle}
-    //                                       weeklyPrice={game.weeklyPrice}
-    //                                       monthlyPrice={game.monthlyPrice}
-    //                                       availableTrophy={game.availableTrophy}
-    //                                       availableNonTrophy={game.availableNonTrophy}
-    //                                       renters={game.renters}
-    //                                       slot={game.slot}
-    //                                       includeButton={true}
-    //                                       includeBadge={false}
-    //                             />
-    //                         )) :
-    //                         games.map((game) => (
-    //                             <GameCard key={game.id}
-    //                                       id={game.id}
-    //                                       gameImage={game.gameImage}
-    //                                       gameTitle={game.gameTitle}
-    //                                       weeklyPrice={game.weeklyPrice}
-    //                                       monthlyPrice={game.monthlyPrice}
-    //                                       availableTrophy={game.availableTrophy}
-    //                                       availableNonTrophy={game.availableNonTrophy}
-    //                                       renters={game.renters}
-    //                                       slot={game.slot}
-    //                                       includeButton={true}
-    //                                       includeBadge={false}
-    //                             />
-    //                         ))
-    //                 }
-    //             </GamesContainer>
-    //         </main>
-    //     </>
-    // )
+    return (
+        <>
+            <main className={'flex flex-col gap-y-6 mt-30 px-3 pb-10 xl:px-0 xl:w-[1280px] xl:mx-auto'}>
+                {/*<AlertBasic title={'Announcement'}*/}
+                {/*            description={['Effective today, we are removing the account deposit requirement. We realized that requiring a deposit goes against our goal of providing an affordable game rental service.',*/}
+                {/*                        'For existing renters who have already paid an account deposit, your deposit will be refunded once your rental period expires.']}*/}
+                {/*/>*/}
+                <h1 className={'font-bold text-4xl'}>Browse Games</h1>
+                <GamesContainer>
+                    {
+                        !session?.user &&
+                        games.map((game: GameDetails) => (
+                            <GameCard key={game.id}
+                                      id={game.id}
+                                      gameImage={game.gameImage}
+                                      gameTitle={game.gameTitle}
+                                      weeklyPrice={game.weeklyPrice}
+                                      monthlyPrice={game.monthlyPrice}
+                                      availableTrophy={game.availableTrophy}
+                                      availableNonTrophy={game.availableNonTrophy}
+                                      renters={game.renters}
+                                      slot={game.slot}
+                                      includeButton={true}
+                                      includeBadge={false}
+                                      isGameReleased={game.isGameReleased}
+                            />
+                        ))
+                    }
+                    {
+                        session?.user &&
+                        notRentedGames.map((game: GameDetails) => (
+                            <GameCard key={game.id}
+                                      id={game.id}
+                                      gameImage={game.gameImage}
+                                      gameTitle={game.gameTitle}
+                                      weeklyPrice={game.weeklyPrice}
+                                      monthlyPrice={game.monthlyPrice}
+                                      availableTrophy={game.availableTrophy}
+                                      availableNonTrophy={game.availableNonTrophy}
+                                      renters={game.renters}
+                                      slot={game.slot}
+                                      includeButton={true}
+                                      includeBadge={false}
+                                      isGameReleased={game.isGameReleased}
+                            />
+                        ))
+                    }
+                </GamesContainer>
+                <p className={'text-center'}>Can&apos;t find your game? <Link href={'https://m.me/1152961824575684'} className={'underline text-blue-500'}>Message us on our Facebook page</Link></p>
+            </main>
+        </>
+    )
 }
